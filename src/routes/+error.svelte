38134dc0cfef58d46c340fde78f98c94e93d4cc1
Gene Marks <gene_marks@outlook.com>
2023-06-19 19:39:18 -0400

    updated error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,15 +1,13 @@
-<div class="mx-6 mt-20 mb-20">
-    <div class="flex flex-col gap-6 items-center">
-        <h1 class="relative flex flex-col text-3xl text-white font-bold uppercase before:absolute before:-bottom-1 before:w-full before:bg-secondary-200 before:h-1">
-            <div>404</div>
-            <div>Not Found</div>
-        </h1>
-        <p>Sorry, this page doesn't exist.</p>
+<div class="mx-6 mt-8 mb-20 md:mt-0">
+    <h1 class="relative w-fit text-3xl text-white font-bold uppercase before:absolute before:-bottom-1 before:w-full before:bg-secondary-200 before:h-1">
+        <div>404</div>
+        <div>Not Found</div>
+    </h1>
+    <p class="mt-4">Sorry, this page doesn't exist.</p>
 
-        <img src="/images/404.gif" alt="page not found" />
+    <img src="/images/404.gif" alt="page not found" class="mt-10" />
 
-        <a href="/" class="flex items-center px-4 h-10 leading-0 text-sm rounded-lg bg-primary-300 hover:bg-primary-200">
-            Go Home
-        </a>
-    </div>
+    <a href="/" class="inline-flex items-center px-4 h-10 mt-6 leading-0 text-sm rounded-lg bg-primary-300 hover:bg-primary-200">
+        Go Home
+    </a>
 </div>
