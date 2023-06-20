0f52f6c8085a833a3d5ba3b9cb51eb72d3927c3f
Gene Marks <gene_marks@outlook.com>
2023-06-20 00:28:00 -0400

    refactored pb hook

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,9 +1,8 @@
-import pb from '$lib/server/pocketbase';
+import { pb } from '$lib/server/pocketbase';
 
 export const handle = async ({ event, resolve }) => {
-
-    event.locals.pb = await pb();
+    event.locals.pb = pb;
     const response = await resolve(event);
-
+    
     return response;
 }
--- src/lib/server/pocketbase.js
+++ src/lib/server/pocketbase.js
@@ -4,15 +4,6 @@ import { POCKETBASE_URL,
     POCKETBASE_ADMIN_PASSWORD
 } from '$env/static/private';
 
-let pb;
-export default async () => {
-    if (pb) {
-        return pb;
-    }
 
-    const newPb = new PocketBase(POCKETBASE_URL);
-    await newPb.collection('users').authWithPassword(POCKETBASE_ADMIN_EMAIL, POCKETBASE_ADMIN_PASSWORD);
-    
-    pb = newPb;
-    return pb;
-};
+export const pb = new PocketBase(POCKETBASE_URL);
+await pb.collection('users').authWithPassword(POCKETBASE_ADMIN_EMAIL, POCKETBASE_ADMIN_PASSWORD);
