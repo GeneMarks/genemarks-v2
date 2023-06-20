c443a1fb3a62e1bab66c2c1b29932f81ecdd0695
Gene Marks <gene_marks@outlook.com>
2023-06-20 10:14:40 -0400

    updated pocketbase url

--- src/lib/server/utils.js
+++ src/lib/server/utils.js
@@ -1,11 +1,11 @@
-import { POCKETBASE_URL } from '$env/static/private';
+import { POCKETBASE_PUBLIC_URL } from '$env/static/private';
 
 export const serializeNonPOJOs = (obj) => {
     return structuredClone(obj);
 };
 
 export const getPocketImageURL = (collectionId, recordId, fileName, size='0x0') => {
-    return `${POCKETBASE_URL}/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`;
+    return `${POCKETBASE_PUBLIC_URL}/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`;
 };
 
 export const shuffle = (arr) => {
