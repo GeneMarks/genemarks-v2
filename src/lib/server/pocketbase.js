da7d70b52c672ff685046a72e9bbc13151cc0746
Gene Marks <gene_marks@outlook.com>
2023-06-21 21:38:36 -0400

    updated for build process

--- src/lib/server/pocketbase.js
+++ src/lib/server/pocketbase.js
@@ -1,10 +1,14 @@
+import { building } from '$app/environment';
 import PocketBase from 'pocketbase';
 import { POCKETBASE_URL,
     POCKETBASE_ADMIN_EMAIL,
     POCKETBASE_ADMIN_PASSWORD
 } from '$env/static/private';
 
+export let pb;
 
-export const pb = new PocketBase(POCKETBASE_URL);
-await pb.collection('users').authWithPassword(POCKETBASE_ADMIN_EMAIL, POCKETBASE_ADMIN_PASSWORD);
-pb.autoCancellation(false);
+if (!building) {
+    pb = new PocketBase(POCKETBASE_URL);
+    await pb.collection('users').authWithPassword(POCKETBASE_ADMIN_EMAIL, POCKETBASE_ADMIN_PASSWORD);
+    pb.autoCancellation(false);
+}
